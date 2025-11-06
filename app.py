07ecc88c3f7c6f7c3492aae29e9c99a0b2c9b525
aswin796 <aswinks6238@gmail.com>
2025-11-06 11:36:38 +0530

    flask

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask,render_template,url_for
+app=Flask(__name__)
+posts=[{'author':'Haro Aso.',
+        'title':'Alice in border land',
+        'date_published':'December 10, 2020'},
+
+        {'author':'George R. R. Martin',
+        'title':'Game Of Thrones',
+        'date_published':'August 1996.'}
+    ]
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('index.html',posts=posts)
+@app.route("/about")
+def about():
+    return render_template('about.html',title="About")
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
